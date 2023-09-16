2af3b8f7f7e2256c3bac943441d1df72df5cfb65
camcast04 <ccast049@fiu.edu>
2023-09-16 18:09:26 -0400

    added chatbot

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ import os
 
 OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 class Student:
-    def _init_(self,first_name,last_name,age,university):
+    def __init__(self,first_name,last_name,age,university):
         self.first_name=first_name
         self.last_name=last_name
         self.age=age
         self.university=university
 
     def display_info(self):
-        print(f"Student Name:{self.first_name}{self.last_name}")
-        print(f"Age: {self.age}")
-        print(f"University: {self.university}")
+       return(f"Student Name:{self.first_name}{self.last_name}"
+              f"Age: {self.age}"
+              f"University: {self.university}")
 
 st.set_page_config(
     page_title = "Financial Literacy",
@@ -44,3 +44,51 @@ student.display_info()
 #Chatbot with gameified financial learning
 
 st.subheader("Financial Queries with ChatGPT")
+
+#session states
+
+if "openai_model" not in st.session_state:
+    st.session_state.messages = []
+
+#Budget initialization
+if 'budget' not in st.session_state:
+    st.session_state.budget = 50000
+
+MAX_BUDGET = 50000
+st.sidebar.write(f"Remaining Budget: ${st.session_state.budget}")
+progress_bar = st.progress(MAX_BUDGET - st.session_state.budget)
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+
+if prompt := st.chat_input("Ask about your finances!"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    full_response = ""
+
+    #AI response with gameified budgeting
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+
+
+    for response in openai.ChatCompletion.create(
+        model = st.session_state["openai_model"],
+        messages = [
+            {"role":m["role"], "content": m["content"]}
+            for m in st.session_state.messages
+        ],
+        stream = True,
+
+    ):
+        full_response += response.choices[0].delta.get("content", "")
+        message_placeholder.markdown(full_response + " ")
+
+    #bidget depletion
+    if "buy" in prompt.lower():
+        depletion_amount = 100
+        st.session_state.budget -= depletion_amount
+        full_response += f"\n\nYou've spent ${depletion_amount}. Remaining budget ${st.session_state.budget}"
+
+    message_placeholder.markdown(full_response)
+st.session_state.messages.append({"role": "assistant", "content": full_response})
